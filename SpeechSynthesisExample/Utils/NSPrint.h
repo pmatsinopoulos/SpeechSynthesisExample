1669fdaa47c7bfabaed1e602cc19059f446f2ce5
Panagiotis Matsinopoulos <panagiotis@matsinopoulos.gr>
2021-07-19 02:51:23 +0300

    Initial Commit

---
+++ SpeechSynthesisExample/Utils/NSPrint.h
@@ -0,0 +1,15 @@
+//
+//  NSPrint.h
+//  SpeechSynthesisExample
+//
+//  Created by Panayotis Matsinopoulos on 19/7/21.
+//
+
+#ifndef NSPrint_h
+#define NSPrint_h
+
+#import <Foundation/Foundation.h>
+
+void NSPrint(NSString *format, ...);
+
+#endif /* NSPrint_h */
--- SpeechSynthesisExample/main.m
+++ SpeechSynthesisExample/main.m
@@ -6,11 +6,96 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <ApplicationServices/ApplicationServices.h>
+#import <pthread/pthread.h>
+
+#import "AppState.h"
+#import "CheckError.h"
+#import "NSPrint.h"
+
+void SpeechDone (SpeechChannel chan, SRefCon refCon) {
+  AppState *appState = (AppState *)refCon;
+  
+  pthread_mutex_lock(&(appState->mutex));
+
+  appState->stopSpeaking = true;
+  pthread_cond_signal(&(appState->cond));
+  
+  pthread_mutex_unlock(&(appState->mutex));
+}
+
+void InitializeSynchronizationState(AppState *appState) {
+  appState->stopSpeaking = false;
+  pthread_mutex_init(&(appState->mutex), NULL);
+  pthread_cond_init(&(appState->cond), NULL);
+}
+
+void SetSpeechCustomData(SpeechChannel channel, AppState *appState) {
+  unsigned long appStateAddress = (unsigned long)appState;
+  appState->appStateRef = CFNumberCreate(kCFAllocatorDefault, kCFNumberLongType, (const void *)&appStateAddress);
+  CheckError(SetSpeechProperty(channel, kSpeechRefConProperty, appState->appStateRef), "Set speech property kSpeechRefConProperty");
+}
+
+void DestroySynchronizationState(AppState *appState) {
+  pthread_mutex_destroy(&(appState->mutex));
+  pthread_cond_destroy(&(appState->cond));
+}
+
+void SetSpeechDoneCallBack(SpeechChannel channel, AppState *appState) {
+  unsigned long speechDoneAddress = (unsigned long)SpeechDone;
+  
+  appState->speechDoneRef = CFNumberCreate(kCFAllocatorDefault, kCFNumberLongType, (const void *)&speechDoneAddress);
+  
+  CheckError(SetSpeechProperty(channel, kSpeechSpeechDoneCallBack, appState->speechDoneRef), "Set speech property kSpeechSpeechDoneCallBack");
+  
+  InitializeSynchronizationState(appState);
+  
+  SetSpeechCustomData(channel, appState);
+}
+
+void UnsetSpeechDoneCallBack(AppState *appState) {
+  CFRelease(appState->speechDoneRef);
+  CFRelease(appState->appStateRef);
+  
+  DestroySynchronizationState(appState);
+}
+
+void WaitSpeechToFinish(AppState *appState) {
+  pthread_mutex_lock(&(appState->mutex));
+  while(!appState->stopSpeaking) {
+    pthread_cond_wait(&(appState->cond), &(appState->mutex));
+  }
+  pthread_mutex_unlock(&(appState->mutex));
+}
 
 int main(int argc, const char * argv[]) {
   @autoreleasepool {
-      // insert code here...
-      NSLog(@"Hello, World!");
+    if (argc < 2) {
+      NSLog(@"1st argument: You need to give the phrase to be spoken out.\n");
+      return 1;
+    }
+    
+    SpeechChannel channel;
+    
+    NewSpeechChannel(NULL, &channel);
+
+    AppState appState;
+    
+    SetSpeechDoneCallBack(channel, &appState);
+        
+    CFStringRef stringToSpeak = CFStringCreateWithCString(kCFAllocatorDefault, argv[1], CFStringGetSystemEncoding());
+    
+    CheckError(SpeakCFString(channel, stringToSpeak, NULL), "Speak CF String");
+    
+    WaitSpeechToFinish(&appState);
+    
+    CheckError(DisposeSpeechChannel(channel), "Dispose speech channel");
+        
+    UnsetSpeechDoneCallBack(&appState);
+    
+    CFRelease(stringToSpeak);
+        
+    NSPrint(@"Bye!\n");
   }
   return 0;
 }
